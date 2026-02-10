0fa67fd16455754cba586634682c2af5412125bc
Emile Sonneveld <emile.sonneveld@vito.be>
2026-02-10 16:51:39 +0100

    run_cwl_to_stac example.

--- force_level2_openeo_test.py
+++ force_level2_openeo_test.py
@@ -1,4 +1,9 @@
+from datetime import datetime
+from pathlib import Path
+
 import openeo
+from openeo.internal.graph_building import PGNode
+from openeo.rest.stac_resource import StacResource
 
 url = "https://openeo.dataspace.copernicus.eu"
 # url = "https://openeo-staging.dataspace.copernicus.eu/"
@@ -8,13 +13,32 @@ connection = openeo.connect(url).authenticate_oidc()
 #     process_id="force_level2",
 # )
 
-datacube = connection.datacube_from_process(
-    process_id="run_cwl",
-    cwl_url="https://raw.githubusercontent.com/EmileSonneveld/apex-force-openeo/refs/heads/main/material/force-l2.cwl",
-    context={},
-    stac_root="catalogue.json",
-)
+cwl = Path("material/force-l2.cwl").read_text()
 
-job = datacube.create_job(title=__file__)
+# With run_udf, the resulting stac catalog will be loaded by openEO:
+# datacube = connection.datacube_from_process(
+#     "run_udf",
+#     data=None,
+#     udf=cwl,
+#     runtime="EOAP-CWL",
+#     context={},
+# )
+
+# With run_cwl_to_stac, the resulting stac catalog can be put in a s3 bucket without any processing:
+stac_resource = StacResource(
+    graph=PGNode(
+        "run_cwl_to_stac",
+        arguments={
+            "cwl_url": cwl,
+            "context": {},
+        },
+    ),
+    connection=connection,
+)
+stac_resource = stac_resource.export_workspace(
+    workspace="tmp_workspace",  # TODO: Use apex force specific workspace.
+    merge=f"{__file__}_{datetime.now().strftime('%Y-%m-%d_%H%M%S')}",
+)
+job = stac_resource.create_job(title=__file__)
 job.start_and_wait()
 results = job.get_results()
